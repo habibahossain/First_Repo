03e1b13f3bf0ab0269db3dffc1032ceea3d77224
Shifa2139 <sayemajahan969@gmail.com>
2023-05-11 11:54:27 +0600

    Function Overloading

---
+++ src/FunctionOverloading/FunctionOverloading.java
@@ -0,0 +1,19 @@
+package FunctionOverloading;
+
+public class FunctionOverloading 
+{
+	public int FunctionName(int x, int y)
+	{
+	return (x + y); 
+	}
+	
+	public int FunctionName(int x, int y, int z)
+	{
+	return (x + y + z);
+	}
+	
+	public double FunctionName(double x, double y)
+	{
+	return (x + y);
+	}
+}
---
+++ src/FunctionOverloading/main1.java
@@ -0,0 +1,15 @@
+package FunctionOverloading;
+
+public class main1 
+{
+
+	public static void main(String[] args) 
+	{
+		FunctionOverloading s = new FunctionOverloading();
+		System.out.println(s.FunctionName(10, 20));
+		System.out.println(s. FunctionName(10, 20, 30));
+		System.out.println(s. FunctionName(10.5, 20.5));
+
+	}
+
+}
